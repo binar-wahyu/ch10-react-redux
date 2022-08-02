38adf4c853cc252f45b963de7f24ff3150c610a5
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-08-02 20:04:00 +0800

    Implement redux

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,9 +1,32 @@
-function PostsPage() {
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../store/actions/postsActions";
+import Post from "../components/Post";
+
+function PostsPage({ dispatch, loading, posts, hasErrors }) {
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  function jsx_posts() {
+    // Show loading, error, or success state
+    if (loading) return <p>Loading posts...</p>;
+    if (hasErrors) return <p>Unable to display posts.</p>;
+    return posts.map((post) => <Post key={post.id} post={post} />);
+  }
+
   return (
     <section>
       <h1>Posts</h1>
+      {jsx_posts()}
     </section>
   );
 }
 
-export default PostsPage;
+const mapStateToProps = (state) => ({
+  loading: state.posts.loading,
+  posts: state.posts.posts,
+  hasErrors: state.posts.hasErrors,
+});
+
+export default connect(mapStateToProps)(PostsPage);
